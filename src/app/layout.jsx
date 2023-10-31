5a1b1f62d8fa3985ee6361739be970232467361f
Katherina <fedoseeva2292@gmail.com>
2023-10-31 11:05:04 +0100

    Favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,11 +6,16 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata = {
   title: 'Ekaterina Fedoseeva',
   description: 'Portfolio',
+  icons: {
+    icon: ['/favicon/favicon.ico'],
+    apple: ['/favicon/apple-touch-icon.png'],
+  },
+  manifest: '/favicon/site.webmanifest',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" style={{ scrollBehavior: 'smooth' }}>
       <body className={inter.className}>{children}</body>
     </html>
   );
